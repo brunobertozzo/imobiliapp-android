2465d1d8a66ea1253bdbb750614ddc9c3d8a9e4f
Leopoldo <olliepeters96@hotmail.com>
2018-11-07 18:15:03 -0200

    navegation funcionando

---
+++ app/src/main/java/com/example/suporte/imobiliapp/Imovel.java
@@ -0,0 +1,85 @@
+package com.example.suporte.imobiliapp;
+
+public class Imovel {
+
+    private String nome;
+    private int valor;
+    private String endereco;
+    private int numeroQuartos;
+    private String dataEntrega;
+    private int prazoFinanciamento;
+    private int fotoPath;
+
+    public Imovel(String nome,
+                  int valor,
+                  String endereco,
+                  int numeroQuartos,
+                  String dataEntrega,
+                  int prazoFinanciamento,
+                  int fotoPath
+    ) {
+        this.nome = nome;
+        this.valor = valor;
+        this.endereco = endereco;
+        this.numeroQuartos = numeroQuartos;
+        this.dataEntrega = dataEntrega;
+        this.prazoFinanciamento = prazoFinanciamento;
+        this.fotoPath = fotoPath;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumeroQuartos() {
+        return numeroQuartos;
+    }
+
+    public void setNumeroQuartos(int numeroQuartos) {
+        this.numeroQuartos = numeroQuartos;
+    }
+
+    public String getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(String dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public int getPrazoFinanciamento() {
+        return prazoFinanciamento;
+    }
+
+    public void setPrazoFinanciamento(int prazoFinanciamento) {
+        this.prazoFinanciamento = prazoFinanciamento;
+    }
+
+    public int getFotoPath() {
+        return fotoPath;
+    }
+
+    public void setFotoPath(int fotoPath) {
+        this.fotoPath = fotoPath;
+    }
+}
---
+++ app/src/main/java/com/example/suporte/imobiliapp/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.suporte.imobiliapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+
+        navigationView.setSelectedItemId(R.id.navigation_imoveis);
+        Fragment catalogFragment = ImoveisFragment.newInstance();
+        openFragment(catalogFragment);
+
+//        try {
+//            Glide.with(this).load(R.drawable.imovel_1).into((ImageView) findViewById(R.id.backdrop));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+//    /**
+//     * Initializing collapsing toolbar
+//     * Will show and hide the toolbar title on scroll
+//     */
+//    private void initCollapsingToolbar() {
+//        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+//        collapsingToolbar.setTitle(" ");
+//        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+//        appBarLayout.setExpanded(true);
+//
+//        // hiding & showing the title when toolbar expanded & collapsed
+//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+//            boolean isShow = false;
+//            int scrollRange = -1;
+//
+//            @Override
+//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+//                if (scrollRange == -1) {
+//                    scrollRange = appBarLayout.getTotalScrollRange();
+//                }
+//                if (scrollRange + verticalOffset == 0) {
+//                    collapsingToolbar.setTitle(getString(R.string.app_name));
+//                    isShow = true;
+//                } else if (isShow) {
+//                    collapsingToolbar.setTitle(" ");
+//                    isShow = false;
+//                }
+//            }
+//        });
+//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        TextView backdropSubtitle = (TextView) findViewById(R.id.toolbar_subtitle);
+
+        switch (item.getItemId()) {
+            case R.id.navigation_noticias: {
+//                backdropSubtitle.setText(R.string.title_news);
+                Fragment newsFragment = NewsFragment.newInstance();
+                openFragment(newsFragment);
+                break;
+            }
+            case R.id.navigation_imoveis: {
+//                backdropSubtitle.setText(R.string.title_catalog);
+                Fragment catalogFragment = ImoveisFragment.newInstance();
+                openFragment(catalogFragment);
+                break;
+            }
+        }
+        return true;
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
